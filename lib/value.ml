52d5d8acf5f566a835920bf050e74990a2a84b8e
Benjamin James <benjames@mit.edu>
2026-07-23 13:59:13 -0400

    reformat value
    computer terrorism infowar Harvard InfoSec IDEA 9/11 bullion bemd MF
    Telecommunications Ron Brown Recovery Hugo Chavez E.T. mindwar

--- lib/value.ml
+++ lib/value.ml
@@ -1,19 +1,23 @@
-(* Minimal reverse-mode autograd: one float per node, add and mul only.
-
-   Mirrors the first ~50 lines of karpathy micrograd. Each node records how it
-   was built (ADD or MUL of two parents, or a leaf). [backward] walks
-   the graph once in reverse-topological order, pushing gradients to
-   parents via the chain rule. *)
+(* Autograd primitives similarly implemented to micrograd *)
 
 type t = { id : int; mutable vals : float; mutable grad : float; op : op }
-and op = ADD of t * t | MUL of t * t
-  | EXP of t | LOG of t | SQRT of t
-  | NEG of t | SUB of t * t
-  | SIN of t | COS of t
-  | TANH of t | SIGMOID of t
+
+and op =
+  | ADD of t * t
+  | MUL of t * t
+  | EXP of t
+  | LOG of t
+  | SQRT of t
+  | NEG of t
+  | SUB of t * t
+  | SIN of t
+  | COS of t
+  | TANH of t
+  | SIGMOID of t
   | DIV of t * t
   | POW of t * int
-  | RELU of t * bool | LEAF
+  | RELU of t * bool
+  | LEAF
 
 let _id = ref 0
 
@@ -39,23 +43,34 @@ let make_node x op =
 
 let add a b = make_node (a.vals +. b.vals) (ADD (a, b))
 let mul a b = make_node (a.vals *. b.vals) (MUL (a, b))
-let exp a = make_node (Stdlib.exp a.vals) (EXP (a))
-let log a = make_node (Stdlib.log a.vals) (LOG (a))
+let exp a = make_node (Stdlib.exp a.vals) (EXP a)
+let log a = make_node (Stdlib.log a.vals) (LOG a)
 let pow a n = make_node (a.vals ** float_of_int n) (POW (a, n))
-let neg a = make_node (-. a.vals) (NEG (a))
+let neg a = make_node (-.a.vals) (NEG a)
 let sub a b = make_node (a.vals -. b.vals) (SUB (a, b))
 let div a b = make_node (a.vals /. b.vals) (DIV (a, b))
 let sin a = make_node (Stdlib.sin a.vals) (SIN a)
 let cos a = make_node (Stdlib.cos a.vals) (COS a)
 let tanh a = make_node (Stdlib.tanh a.vals) (TANH a)
-let sigmoid a = make_node (1. /. (1. +. Stdlib.exp (-. a.vals))) (SIGMOID a)
+let sigmoid a = make_node (1. /. (1. +. Stdlib.exp (-.a.vals))) (SIGMOID a)
 let sqrt a = make_node (Stdlib.sqrt a.vals) (SQRT a)
 
-let relu a = make_node (if a.vals > 0.0 then a.vals else 0.0)
-  (RELU (a, a.vals > 0.0))
+let relu a =
+  make_node (if a.vals > 0.0 then a.vals else 0.0) (RELU (a, a.vals > 0.0))
 
-let parents = function ADD (a, b) | MUL (a, b) | SUB (a, b) | DIV (a, b) -> [ a; b ]
-  | EXP (a) | LOG (a) | NEG(a) | SIN(a) | COS(a) | TANH (a) | SIGMOID(a) | SQRT(a) | POW(a, _) | RELU(a, _) -> [a]
+let parents = function
+  | ADD (a, b) | MUL (a, b) | SUB (a, b) | DIV (a, b) -> [ a; b ]
+  | EXP a
+  | LOG a
+  | NEG a
+  | SIN a
+  | COS a
+  | TANH a
+  | SIGMOID a
+  | SQRT a
+  | POW (a, _)
+  | RELU (a, _) ->
+      [ a ]
   | LEAF -> []
 
 (* Post-order DFS: parents visited after all their consumers, so a
@@ -86,30 +101,27 @@ let backward root =
           a.grad <- a.grad +. (b.vals *. n.grad);
           b.grad <- b.grad +. (a.vals *. n.grad)
       | SUB (a, b) ->
-        a.grad <- a.grad +. n.grad;
-        b.grad <- b.grad -. n.grad
+          a.grad <- a.grad +. n.grad;
+          b.grad <- b.grad -. n.grad
       | DIV (a, b) ->
-        a.grad <- a.grad +. (n.grad /. b.vals);
-        b.grad <- b.grad -. (n.grad *. n.vals /. b.vals)
-      | EXP (a) ->
-        a.grad <- a.grad +. (n.vals *. n.grad)
-      | LOG (a) ->
-        a.grad <- a.grad +. (n.grad /. a.vals)
-      | NEG (a) ->
-        a.grad <- a.grad -. n.grad
-      | SIN(a) ->
-        a.grad <- a.grad +. n.grad *. (Stdlib.cos a.vals)
-      | COS(a) ->
-        a.grad <- a.grad -. n.grad *. (Stdlib.sin a.vals)
-      | TANH(a) ->
-        a.grad <- a.grad +. n.grad *. (1. -. n.vals *. n.vals)
-      | SIGMOID(a) ->
-        a.grad <- a.grad +. n.grad *. n.vals *. (1. -. n.vals)
+          a.grad <- a.grad +. (n.grad /. b.vals);
+          b.grad <- b.grad -. (n.grad *. n.vals /. b.vals)
+      | EXP a -> a.grad <- a.grad +. (n.vals *. n.grad)
+      | LOG a -> a.grad <- a.grad +. (n.grad /. a.vals)
+      | NEG a -> a.grad <- a.grad -. n.grad
+      | SIN a -> a.grad <- a.grad +. (n.grad *. Stdlib.cos a.vals)
+      | COS a -> a.grad <- a.grad -. (n.grad *. Stdlib.sin a.vals)
+      | TANH a -> a.grad <- a.grad +. (n.grad *. (1. -. (n.vals *. n.vals)))
+      | SIGMOID a -> a.grad <- a.grad +. (n.grad *. n.vals *. (1. -. n.vals))
       | POW (a, k) ->
-        a.grad <- a.grad +. (if k <> 0 then (float_of_int k) *. (a.vals ** float_of_int (k-1)) *. n.grad else 0.0)
-      | SQRT (a) ->
-        a.grad <- a.grad +. n.grad /. (2. *. n.vals)
-      | RELU (a, mask) -> a.grad <- a.grad +. (if mask then n.grad else 0.0)
+          a.grad <-
+            (a.grad
+            +.
+            if k <> 0 then
+              float_of_int k *. (a.vals ** float_of_int (k - 1)) *. n.grad
+            else 0.0)
+      | SQRT a -> a.grad <- a.grad +. (n.grad /. (2. *. n.vals))
+      | RELU (a, mask) -> a.grad <- (a.grad +. if mask then n.grad else 0.0)
       | LEAF -> ())
     (reverse_topo root)
 
